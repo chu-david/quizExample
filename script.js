603007030dca01dae74ebe7e36acb79a1c45bbe8
chu-david <dchu@emmauscc.sa.edu.au>
2018-11-27 10:10:13 +1030

    Added timer. Test

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
 
+//Lesson 2
 
-console.log("Hello World");
+/*
 
-function foo(){
-	
-	console.log("foo");
-	
-}
+function checkAnswers(){
 
-function fa(){
-	
-	
-	
-}
+		var correctAnswer1 = 'q1Option1';
+
+		if(document.getElementById(correctAnswer1).checked == true){
+
+				console.log("Question 1 Correct");
+
+		} else{
+
+				console.log("Question 1 Incorrect");
+
+		}//end if statement
+
+} //end function //
+
+*/
+var interval;
+
+var i=0;
+
+interval = setInterval(function(){
+
+	i=i+1;
+	console.log(i);
+
+})
+
+function checkAnswers(){
+
+				var correctAnswer = [1, 2, 4, 2];
+
+				var counter = 0;
+
+		for(var i=0;i<4;i++){//loop for each question
+				console.log('q'+(i+1)+'Option'+correctAnswer[i]);
+				if(document.getElementById('q'+(i+1)+'Option'+correctAnswer[i]).checked ==true){
+
+					counter = counter + 1;
+
+					document.getElementById('Question'+(i+1)).style.color = 'green';
+
+				}
+
+				for(var j=0;j<4;j++){
+				document.getElementsByName('q'+(i+1))[j].disabled=true;
+				}
+		}
+
+						console.log("score = "+ counter);
+	}
